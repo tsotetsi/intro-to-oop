d14ed3fef8dd5a5b45351fdcd1cb22d42bab036b
Tsotetsi <me@tsotetsithapeloo.co.za>
2021-05-18 14:28:20 +0200

    Make test pass. Add .gitignore file.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # intro-to-oop [![Build Status](https://travis-ci.org/tsotetsi/intro-to-oop.svg?branch=main)](https://travis-ci.org/tsotetsi/intro-to-oop)
+
--- driver.py
+++ driver.py
@@ -3,6 +3,5 @@ from main import Bus
 
 bus_one = Bus(10, "Red", "Thapelo")
 Bus(10, "Red", "Thapelo")
-Bus(10, "Red", "Thapelo")
 print(bus_one.color)
 print(Bus.num_bus_created)
